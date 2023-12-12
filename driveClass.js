cbfe7715322c3b35c354096c8f35bdd9f578c89c
wael <waelferjeni86@gmail.com>
2023-12-12 10:29:48 +0100

    firstcommit

---
+++ driveClass.js
@@ -0,0 +1,10 @@
+const Driver = class {
+    constructor(id,fullName, gender, BD_year){
+        this.id=idthis.fullName=fullName
+        this.gender=gender
+        this.BD_year=BD_year
+    }
+    ages(){
+        return new Date().getFullYear()-this.since
+    }
+} 
---
+++ passengerClass.js
@@ -0,0 +1,10 @@
+const Passenger = class {
+    constructor(id,fullName, gender, BD_year){
+        this.id=idthis.fullName=fullName
+        this.gender=gender
+        this.BD_year=BD_year
+    }
+    ages(){
+        return new Date().getFullYear()-this.since
+    }
+} 
